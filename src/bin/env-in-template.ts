2e745d83819d12c8f48aa1dd21fb49a33d3682f8
Morten Sjøgren <m_abs@mabs.dk>
2020-03-27 12:47:54 +0100

    update deps and throw error on error

--- src/bin/env-in-template.ts
+++ src/bin/env-in-template.ts
@@ -11,27 +11,36 @@ const fsReadFile = promisify(fs.readFile);
 const fsWriteFile = promisify(fs.writeFile);
 
 const argv = minimist(process.argv.slice(2));
-async function worker() {
-  if (argv._.length !== 2) {
-    // tslint:disable-next-line:no-console
-    console.warn(`Usage: ${path.basename(process.argv[0])} input-file output-file`);
-    process.exit(127);
-    return;
-  }
 
+if (argv._.length !== 2) {
+  // tslint:disable-next-line:no-console
+  console.warn(
+    `Usage: ${path.basename(process.argv[0])} input-file output-file`,
+  );
+  process.exit(127);
+}
+
+async function worker() {
   const [inputFile, outputFile] = argv._;
 
   if (!(await fsExists(inputFile))) {
     console.error(`${inputFile} doesn't exist.`);
     process.exit(127);
-    return;
   }
 
-  const inputData = await fsReadFile(inputFile, 'UTF-8');
-  const template = t(inputData);
+  try {
+    const inputData = await fsReadFile(inputFile, 'UTF-8');
+    const template = t(inputData);
 
-  const outputData = template(process.env);
-  fsWriteFile(outputFile, outputData, 'UTF-8');
+    const outputData = template(process.env);
+    await fsWriteFile(outputFile, outputData, 'UTF-8');
+  } catch (err) {
+    console.error(err);
+
+    process.exit(1);
+  }
+
+  process.exit(0);
 }
 
 worker();
